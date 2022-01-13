a94933b67c01c836767ba6a1753cbb5f1686430b
Papus <foal0912@gmail.com>
2022-01-13 10:18:52 +0900

    두 번 째

--- pybo/views.py
+++ pybo/views.py
@@ -115,4 +115,19 @@ def answer_modify(request, answer_id):
     Answer 내용 수정
     """
     answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정 권한이 없습니다.')
+        return redirect('pybo:detail', question_id=answer.question.id)
+
     if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'pybo/answer_form.html', context)
